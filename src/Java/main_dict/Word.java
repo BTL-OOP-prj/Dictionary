f4e8d13130be7961b662ce176fac4b86e87d75f0
Hoàng Công Vinh <haxorus5224@gmail.com>
2023-10-09 16:26:56 +0700

    add feature import from DB to Trie

---
+++ src/Java/main_dict/Word.java
@@ -0,0 +1,82 @@
+package main_dict;
+
+public class Word {
+    private String Content;
+    private String Type;
+    private String Meaning;
+    private String Example;
+
+    /**
+     * Constructor.
+     */
+    public Word(String Content, String Type, String Meaning, String Example) {
+        this.Content = Content;
+        this.Type = Type;
+        this.Meaning = Meaning;
+        this.Example = Example;
+    }
+
+    /**
+     * Content Getter.
+     */
+    public String getContent() {
+        return Content;
+    }
+
+    /**
+     * Content Setter.
+     */
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
+
+    /**
+     * Type Getter.
+     */
+    public String getType() {
+        return Type;
+    }
+
+    /**
+     * Type Setter.
+     */
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    /**
+     * Meaning Getter.
+     */
+    public String getMeaning() {
+        return Meaning;
+    }
+
+    /**
+     * Meaning Setter.
+     */
+    public void setMeaning(String Meaning) {
+        this.Meaning = Meaning;
+    }
+
+    /**
+     * Example Getter.
+     */
+    public String getExample() {
+        return Example;
+    }
+
+    /**
+     * Example Setter.
+     */
+    public void setExample(String Example) {
+        this.Example = Example;
+    }
+
+    /**
+     * toString.
+     */
+    @Override
+    public String toString() {
+        return "Word [Content=" + Content + ", Type=" + Type + ", Meaning=" + Meaning + ", Example=" + Example + "]";
+    }
+}
---
+++ src/Java/main_dict/dbToManager.java
@@ -0,0 +1,105 @@
+package main_dict;
+
+import java.io.*;
+import java.util.Scanner;
+import main_dict.Word;
+import main_dict.WordsManager;
+
+public class dbToManager {
+
+    /**
+     * split.
+     */
+    public static String[] split(String str, String delimiter) {
+        String[] result = new String[4];
+        int index = str.indexOf(delimiter);
+        //check content
+        if (index == -1) {
+            result[0] = "";
+        }
+        else {
+            result[0] = str.substring(0, index);
+            str = str.substring(index + 1);
+        }
+        //check type
+        index = str.indexOf(delimiter);
+        if (index == -1) {
+            result[1] = "";
+        }
+        else {
+            result[1] = str.substring(0, index);
+            str = str.substring(index + 1);
+        }
+        //check meaning
+        index = str.indexOf(delimiter);
+        if (index == -1) {
+            result[2] = "";
+        }
+        else {
+            result[2] = str.substring(0, index);
+            str = str.substring(index + 1);
+        }
+        //check example
+        index = str.indexOf(delimiter);
+        if (index == -1) {
+            result[3] = "";
+        }
+        else {
+            result[3] = str.substring(0, index);
+            str = str.substring(index + 1);
+        }
+        return result;
+    }
+
+    /**
+     * Scan the database and add all words to the manager.
+     */
+    public static void scan(String path) {
+        try {
+            Scanner sc = new Scanner(new File(path));
+            sc.nextLine();
+            sc.useDelimiter(",|\n");
+            while (sc.hasNextLine()) {
+                String curLine = sc.nextLine();
+                String[] curWord = split(curLine, ",");
+                String Content = curWord[0];
+                String Type = curWord[1];
+                String Meaning = curWord[2];
+                String Example = curWord[3];
+                if (Content.indexOf(" ") != -1 || Content.indexOf("-") != -1 || Content.indexOf("'") != -1 ||
+                    Content.indexOf(".") != -1 || Content.indexOf(",") != -1 || Content.indexOf("?") != -1 ||
+                    Content.indexOf("!") != -1 || Content.indexOf(":") != -1 || Content.indexOf(";") != -1 ||
+                    Content.indexOf("\"") != -1 || Content.indexOf("(") != -1 || Content.indexOf(")") != -1 ||
+                    Content.indexOf("[") != -1 || Content.indexOf("]") != -1 || Content.indexOf("{") != -1 ||
+                    Content.indexOf("}") != -1 || Content.indexOf("/") != -1 || Content.indexOf("\\") != -1 ||
+                    Content.indexOf("<") != -1 || Content.indexOf(">") != -1 || Content.indexOf("|") != -1 ||
+                    Content.indexOf("*") != -1 || Content.indexOf("&") != -1 || Content.indexOf("^") != -1 ||
+                    Content.indexOf("%") != -1 || Content.indexOf("$") != -1 || Content.indexOf("#") != -1 ||
+                    Content.indexOf("@") != -1 || Content.indexOf("~") != -1 || Content.indexOf("`") != -1 ||
+                    Content.indexOf("+") != -1 || Content.indexOf("=") != -1 || Content.indexOf("-") != -1 ||
+                    Content.indexOf("_") != -1 || Content.indexOf("0") != -1 || Content.indexOf("1") != -1 ||
+                    Content.indexOf("2") != -1 || Content.indexOf("3") != -1 || Content.indexOf("4") != -1 ||
+                    Content.indexOf("5") != -1 || Content.indexOf("6") != -1 || Content.indexOf("7") != -1 ||
+                    Content.indexOf("8") != -1 || Content.indexOf("9") != -1) {
+                    continue;
+                }
+                Word word = new Word(Content, Type, Meaning, Example);
+                WordsManager.insertWord(word);
+                System.out.println(word.toString());
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        
+    }
+
+    /**
+     * main.
+     */
+    public static void main(String[] args) {
+        scan("E:/Dictionary/src/DB/Eng.csv");
+    }
+
+}
